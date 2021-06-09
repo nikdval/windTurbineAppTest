44ea077026e21f5e5961ef65d1bb4ba6bd3e75c4
Valia <valia.nikolakopoulou@controllis.com>
2021-06-09 20:15:00 +0100

    Turbine icon

--- src/pages/Turbine/Turbine.js
+++ src/pages/Turbine/Turbine.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import {generateDetails, generateAlarms} from '../../store/turbineActions';
 import Alarms from '../../components/Alarms/Alarms';
 import DetailsTable from '../../components/DetailsTable/DetailsTable';
+import CustomIcon from '../../components/CustomIcon/CustomIcon';
+
 import './Turbine.scss';
 
 const Turbine = (props) => {
@@ -31,9 +33,14 @@ const Turbine = (props) => {
     // When loading or no data available
     if (!isData) return <div>Please wait, loading.</div>;
    
+    const iconColor = alarms.length > 0 ? '#FF1D25' : '#FACD3C'
     return (
         <div className="turbine">
-            <h2>{details.name}</h2>
+            <div className="turbine-title-wrapper">
+                <CustomIcon name="windTurbine" classes="turbine-icon" color={iconColor}/>
+                <h2>{details.name}</h2>
+            </div>
+
             <DetailsTable details={details}/>
             <Alarms alarms={alarms} />
         </div>
